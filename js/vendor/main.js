996f1c29ef4048af849d736a58e7a1eff5dabf49
Nathan-Jays256 <ssebagalanathan2@gmail.com>
2021-04-29 23:38:44 +0300

    changes Thurs

--- js/vendor/main.js
+++ js/vendor/main.js
@@ -41,6 +41,43 @@ animateIn: 'fadeIn',
       }
   }
 });
+    // review-active
+$('.slider_active_info').owlCarousel({
+  loop:true,
+  margin:0,
+items:1,
+autoplay:true,
+navText:['<i class="ti-angle-left"></i>','<i class="ti-angle-right"></i>'],
+  nav:true,
+dots:false,
+autoplayHoverPause: true,
+autoplaySpeed: 800,
+animateOut: 'fadeOut',
+animateIn: 'fadeIn',
+  responsive:{
+      0:{
+          items:1,
+          dots:false,
+          nav:false,
+      },
+      767:{
+          items:1,
+          dots:false,
+          nav:false,
+      },
+      992:{
+          items:1,
+          nav:false
+      },
+      1200:{
+          items:3,
+          nav:false
+      },
+      1500:{
+          items:3
+      }
+  }
+});
 
 
 
